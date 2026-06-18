621f75d53f88ac2960f65f59eb40d360cf2c97e4
Sheikh Naim <naimbgd@gmail.com>
2026-06-18 09:44:06 -0400

    Initial Commit

---
+++ Hydration Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hydration Watch App
+//
+//  Created by Sheikh Naim on 2026-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hydration Watch App/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Hydration Watch App
+//
+//  Created by Sheikh Naim on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct Hydration_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
